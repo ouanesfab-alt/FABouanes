044115aa2a96a349caa6e1830d274a72d4825c26
massi <massi@example.com>
2026-07-10 04:56:23 +0200

    style: increase navbar clock font-size to 30px

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "fabouanes-v45-offline";
+const VERSION = "fabouanes-v46-offline";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const OFFLINE_URL = "/static/offline.html";
